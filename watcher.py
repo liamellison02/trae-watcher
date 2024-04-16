1159ea73544f65f1294e92ac366dcdd577896ed6
Liam Ellison <liam.ellison.codes@gmail.com>
2024-04-15 23:40:22 -0400

    Stashing from PC

--- watcher.py
+++ watcher.py
@@ -11,73 +11,75 @@ from confluent_kafka.serialization import StringSerializer
 from confluent_kafka.schema_registry.json_schema import JSONSerializer
 from confluent_kafka.serializing_producer import SerializingProducer
 
-
 base_url = "https://www.reddit.com"
 endpoint = "/r/AtlantaHawks"
 request_category = "/search.json"
-specific_keys = ['name', 'subreddit_id', 'author', 'author_fullname', 'created_utc', 'title', 'ups', 'score', 'num_comments', 'total_awards_received', 'upvote_ratio', 'permalink']
+specific_keys = ['name', 'subreddit_id', 'author', 'author_fullname', 'created_utc', 'title', 'ups', 'score',
+                 'num_comments', 'total_awards_received', 'upvote_ratio', 'permalink']
+
 
 def get_url(query, limit, time, sort):
-	url = base_url + endpoint + request_category + "?" + f"q={query}" + f"&limit={limit}"+ f"&t={time}"+ f"&sort={sort}"+ "&restrict_sr=on"
-	return url
-	
-	
+    url = base_url + endpoint + request_category + "?" + f"q={query}" + f"&limit={limit}" + f"&t={time}" + f"&sort={sort}" + "&restrict_sr=on"
+    return url
+
+
 def get_search_results(url):
-	try:
-		res = requests.get(url)
-		return res.json()
-	except Exception as e:
-		logging.info(e)
+    try:
+        res = requests.get(url)
+        return res.json()
+    except Exception as e:
+        logging.info(e)
 
 
-def clean_data(json_data):
-	data = json_data['data']['children']
+def clean_data(json_data: dict):
+    listings = json_data['data']['children']
+    listings.get(key) for key in specific_keys
 
-	df_data = pd.Series()
 
-	for item in data:
-		item_data = item['data']
-		row = [item_data.get(key, np.nan) for key in specific_keys]
-		
+    df_data = pd.Series()
 
-	return pd.DataFrame(df_rows, columns=specific_keys)
+    for item in data:
+        item_data = item['data']
+        pd.Series(data=[item_data.get(key, np.nan) for key in specific_keys], index=specific_keys)
+
+    return pd.DataFrame(df_data, columns=specific_keys)
 
 
 def on_delivery(err, record):
-	pass
+    pass
 
 
 def main():
-	logging.info("START")
+    logging.info("START")
 
-	schema_registry_client = SchemaRegistryClient(config["schema_registry"])
-	reddit_listings_schema = schema_registry_client.get_latest_version("reddit_listings")
+    schema_registry_client = SchemaRegistryClient(config["schema_registry"])
+    reddit_listings_schema = schema_registry_client.get_latest_version("reddit_listings")
 
-	kafka_config = config["kafka"] | {
-		"key.serializer": StringSerializer(),
-		"value.serializer": JSONSerializer(schema_registry_client, reddit_listings_schema.schema.schema_str)
-	}
+    kafka_config = config["kafka"] | {
+        "key.serializer": StringSerializer(),
+        "value.serializer": JSONSerializer(schema_registry_client, reddit_listings_schema.schema.schema_str)
+    }
 
-	producer = SerializingProducer(kafka_config)
+    producer = SerializingProducer(kafka_config)
 
-	res = get_search_results(get_url("trae+young", "100", "year", "new"))
-	df = clean_data(res)
-	identifiers = df.loc[:, 'name']
-	for name in identifiers:
-		producer.produce(
-			topic="reddit_listings",
-			key=name,
-			value={
-				f"": ""
-				
-			},
-			on_delivery=on_delivery
-			)
+    res = get_search_results(get_url("trae+young", "100", "year", "new"))
+    df = clean_data(res)
+    identifiers = df.loc[:, 'name']
+    for name in identifiers:
+        producer.produce(
+            topic="reddit_listings",
+            key=name,
+            value={
+                f"": ""
 
-	logging.debug(f"Got {clean_data(res)}")
-	producer.flush()
+            },
+            on_delivery=on_delivery
+        )
+
+    logging.debug(f"Got {clean_data(res)}")
+    producer.flush()
 
 
 if __name__ == "__main__":
-	logging.basicConfig(level=logging.DEBUG)
-	sys.exit(main())
+    logging.basicConfig(level=logging.DEBUG)
+    sys.exit(main())
